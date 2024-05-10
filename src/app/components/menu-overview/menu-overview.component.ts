6bdb946c76685425f34d68bea43d9aa8aa09e0fd
Denisa Kita <denisakita7@gmail.com>
2024-05-11 00:27:27 +0200

    create a menu overview according to the provided design

--- src/app/components/menu-overview/menu-overview.component.ts
+++ src/app/components/menu-overview/menu-overview.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestaurantService} from "../../services";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-menu-overview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-overview.component.html',
   styleUrl: './menu-overview.component.css'
 })
-export class MenuOverviewComponent {
+export class MenuOverviewComponent implements OnInit {
+  dishes: any[] = [];
+  selectedTag: string = '';
+  filteredDishes: any[] = [];
+  basketItems: any[] = [];
 
+
+  constructor(private restaurantService: RestaurantService) {
+  }
+
+  ngOnInit() {
+    this.basketItems = this.restaurantService.basketItems;
+    this.getMenu();
+  }
+
+  getMenu(): void {
+    this.restaurantService.getDishes().subscribe(dishes => {
+      this.dishes = dishes;
+      this.filteredDishes = this.dishes;
+    });
+  }
+
+  filterByTag(tag: string) {
+    this.selectedTag = tag;
+
+    if (this.selectedTag === 'Popular') {
+      this.filteredDishes = [...this.dishes];
+    } else {
+      this.filteredDishes = this.dishes.filter(dish => dish.category === tag);
+    }
+  }
+
+  addToBasket(dish: any): void {
+
+  }
+
+  isAddedToBasket(dish: any): boolean {
+    return false
+  }
+
+  getQuantityInBasket(dish: any): number {
+    return 0;
+  }
+
+  decreaseQuantity(dish: any): void {
+
+  }
+
+  increaseQuantity(dish: any): void {
+
+  }
 }
