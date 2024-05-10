792a1653a28bafdbe188a353f9bcae6df1ada66e
Denisa Kita <denisakita7@gmail.com>
2024-05-10 22:43:18 +0200

    create first components and service

---
+++ src/app/components/index.ts
@@ -0,0 +1,9 @@
+import {BasketComponent} from "./basket/basket.component";
+import {MenuOverviewComponent} from "./menu-overview/menu-overview.component";
+
+export const components: any[] = [
+  BasketComponent, MenuOverviewComponent
+]
+
+export * from './basket/basket.component';
+export * from './menu-overview/menu-overview.component';
