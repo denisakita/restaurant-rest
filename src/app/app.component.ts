36f51026643c7131da32aeeb231d5631161b68aa
Denisa Kita <denisakita7@gmail.com>
2024-05-10 23:14:43 +0200

    separate images and create ui for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
+import {MenuOverviewComponent} from "./components";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MenuOverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getDishes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/dishes`);
+  }
+
+  getDishById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/dishes/${id}`);
+  }
+
+  getBasket(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/basket`);
+  }
+
 }
